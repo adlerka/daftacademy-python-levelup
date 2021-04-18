3161ea4b161843cf18fdcdabf1b40a4111a723fb
adlerka <rydzkowska.ania@gmail.com>
2021-04-18 20:16:30 +0200

    task 1.2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 app.counter = 0
@@ -18,3 +18,13 @@ def hello_name_view(name: str):
 def counter():
     app.counter += 1
     return str(app.counter)
+
+
+@app.get("/method", status_code=200)
+@app.put("/method", status_code=200)
+@app.options("/method", status_code=200)
+@app.delete("/method", status_code=200)
+@app.post("/method", status_code=201)
+def read_method(request: Request):
+    return {"method": request.method}
+
