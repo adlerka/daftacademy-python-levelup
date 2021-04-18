03ac3e0f232d386c7a4f888e59d21b336a4dbc6f
adlerka <rydzkowska.ania@gmail.com>
2021-04-18 19:45:04 +0200

    implement endpoints /hello/{name} and /counter

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+app.counter = 0
 
 
 @app.get("/")
 def root():
     return {"message": "Hello world!"}
+
+
+@app.get("/hello/{name}")
+def hello_name_view(name: str):
+    return f"Hello {name}"
+
+
+@app.get("/counter")
+def counter():
+    app.counter += 1
+    return str(app.counter)
